d8091ba8f00d72b590200762d931815017726bef
kpindur <kotaro.pindur@gmail.com>
2024-12-07 07:45:56 +0900

    chore(day6&day7): prepare directories for day6 and day7

---
+++ src/day7/part1.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+type Int = i32;
+
+pub fn load_data(path: &str) -> Result<Vec<Int>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+
+    return Ok(Vec::new());
+}
+
+pub fn part1(path: &str) -> Result<Int, Box<dyn Error>> {
+    let _ = load_data(path)?;
+
+    let result = 0;
+
+    return Ok(result);
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_part1_works() -> Result<(), Box<dyn Error>> {
+        let current_file = file!();
+        let current_dir = PathBuf::from(current_file);
+
+        let prefix = current_dir.parent().expect("Something went terribly wrong...");
+
+        let result = part1(prefix.join("test.dat").to_str().expect("File not found!"))?;
+
+        assert_eq!(0, result);
+
+        return Ok(());
+    }
+}
